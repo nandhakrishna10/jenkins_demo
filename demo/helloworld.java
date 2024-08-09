d132652d26de54e4e61f64047ec7c6e6e62d9ba5
unknown <3122215002067@ITLAB.SSN>
2024-08-09 09:46:57 +0530

    Third Commit

--- demo/helloworld.java
+++ demo/helloworld.java
@@ -3,5 +3,8 @@ class firstcode{
     System.out.println("Hello World");
     System.out.println("Checking whether jenkins automatically pulls from github and builds the file");
     System.out.println(10+10);
+    for (i =0; i<10 ; i++){
+      System.out.println(i);
+    }
   }
 }
