bcd6845113e14eb2ec600be953353c72f72b1265
unknown <3122215002067@ITLAB.SSN>
2024-08-09 09:40:52 +0530

    Second Commit

--- demo/helloworld.java
+++ demo/helloworld.java
@@ -1,5 +1,7 @@
 class firstcode{
   public static void main(String args[]){
     System.out.println("Hello World");
+    System.out.println("Checking whether jenkins automatically pulls from github and builds the file")
+    System.out.println(10+10)
   }
 }
